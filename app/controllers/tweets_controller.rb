823779285d5be3b23823ca2a61eaccec428690ea
pete3249 <katelynrpeterson16@gmail.com>
2020-09-02 14:21:09 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,26 +9,36 @@ get '/tweets' do
     end 
 end 
 
-get '/tweets/:id' do
-     if logged_in?
-         @user = current_user
-         erb :'/tweets/index'
-     else
-         redirect '/login'
-     end 
- end
-
  get '/tweets/new' do
     if logged_in?
-        @user = current_user
         erb :'/tweets/new'
     else 
         redirect '/login'
     end
  end
 
+ get '/tweets/:id' do
+    if logged_in?
+        @user = current_user
+        erb :'/tweets/index'
+    else
+        redirect '/login'
+    end 
+end
+
  post '/tweets' do
-    
+    @user = current_user
+    if logged_in?
+        if !@params[:content].empty?
+            @tweet = @user.tweets.build(params)
+            @tweet.save
+            redirect '/tweets'
+        else
+            redirect '/tweets/new'
+        end
+    else 
+        redirect '/tweets'
+    end 
  end
 
 end

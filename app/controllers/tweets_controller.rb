44c67baf0796dd4008758c5a7606256c9c4d56cc
pete3249 <katelynrpeterson16@gmail.com>
2020-09-02 15:11:45 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -65,7 +65,12 @@ patch '/tweets/:id' do
 end
 
 delete '/tweets/:id' do
-
+    tweet = current_user.tweets.find_by(id: params[:id])
+    if tweet && tweet.destroy
+        redirect '/tweets'
+    else
+        redirect "/tweets/#{@tweet.id}"
+    end
 end
 
 end
